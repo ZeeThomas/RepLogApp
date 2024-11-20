b1533493fc8dc69b93b05c4430251858cfde49d9
Zee <missz91102@gmail.com>
2024-11-20 13:53:03 -0500

    Added css and all login options functioning

--- src/pages/log-signin/loginForm.jsx
+++ src/pages/log-signin/loginForm.jsx
@@ -1,8 +1,12 @@
+import "../../App.css"
+import logo from '../../assets/exercise.png'; // Tell webpack this JS file uses this image
 import {useForm} from "react-hook-form";
-import {app} from "../../config/firebase";
-import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import {app, auth, provider} from "../../config/firebase";
+import {getAuth, signInWithEmailAndPassword, signInWithPopup} from "firebase/auth";
+import {useNavigate} from "react-router-dom";
 export const LoginForm = () => 
 {
+    const navigate = useNavigate();
     const{register, handleSubmit,formState: {errors},} =useForm();
     const OnLogin = async(data) =>
     {
@@ -15,20 +19,32 @@ export const LoginForm = () =>
                 const user = userCredential.user;
             });
             console.log("success!");
+            navigate("/")
         }
         catch(err)
         {
             console.log(err);
         }
     };
+    const signInWithGoogle = async(data) =>
+    {
+        const result = await signInWithPopup(auth, provider);
+        navigate("/");
+    };
     return (
-        <div className="loginForm">
-        <h2>RepLog</h2>
-        <form onSubmit={handleSubmit(OnLogin)}>
-          <input placeholder="Email" {...register("email", {required:true})} />
-          <input placeholder="Passwrod" {...register("password", {required:true})} />
-          <input type="submit" />
+        <div className="loginFormPage">
+        <img className="logo" src={logo}/>
+        <h2 className="title">RepLog</h2>
+        <form className="loginForm" onSubmit={handleSubmit(OnLogin)}>
+            <p>Email</p>
+            <input placeholder="Email" {...register("email", {required:true})} />
+            <p>Password</p>
+            <input placeholder="Password" {...register("password", {required:true})} />
+            <button className="googleAuthButton" onClick={signInWithGoogle}>Login with Google</button>
+            <input className="submitButton" type="submit" value="Login" />
+            <p className="signUpText">Don't have an account yet? <a className="signUpLink"href="/">Sign Up</a></p>
         </form>
+        
     </div>
     );
 }
