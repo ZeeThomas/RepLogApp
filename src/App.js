66615a18b81bbe38abf1a78c5baa4bcaeb43d956
Zee <missz91102@gmail.com>
2025-02-06 17:46:00 -0500

    fixing deployment issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import {Home} from "./pages/home/home"
 import {Login} from "./pages/log-signin/login"
 import {SignUp} from "./pages/log-signin/signUp"
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router basename="https://zeethomas.github.io/RepLogApp/">
+      <Router >
         <UserContext.Provider value={{user: user, setUser:setUser}}>
         <WorkoutContext.Provider value={{workouts: workouts, setWorkouts:setWorkouts}}>
         <Routes>
